61b234dc62aa2d96c74e1d1a10556d2130848612
Rene Mark Barrow <72480653+rb-creator@users.noreply.github.com>
2023-01-25 20:34:34 +0000

    Created MyFirstScript and Volume Controller Scripts

--- Assets/MuffinClicker/Scripts/MyFirstScript.cs
+++ Assets/MuffinClicker/Scripts/MyFirstScript.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class MyFirstScript : MonoBehaviour
 {
+    private int _counter = 0;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMuffinClicked()
     {
         
+        _counter = _counter + 1;
+        Debug.Log(_counter);
     }
 }
---
+++ Assets/MuffinClicker/Scripts/VolumeController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour
+{
+   void OnVolumeChanged()
+    {
+
+    }
+
+}
