638325e9183deab4f8cd10d2531af74ee1cd5586
Rene Mark Barrow <72480653+rb-creator@users.noreply.github.com>
2023-01-26 12:36:28 +0000

    Completed Volume Slider, Total Muffin Updater and Critical Chance based on Random.Value

--- Assets/MuffinClicker/Scripts/MyFirstScript.cs
+++ Assets/MuffinClicker/Scripts/MyFirstScript.cs
@@ -7,15 +7,20 @@ using TMPro;
 
 public class MyFirstScript : MonoBehaviour
 {
-    TextMeshProUGUI m_TextMeshProUGUI;
-
+    [SerializeField]
+    private TextMeshProUGUI _totalMuffinsText;
     [SerializeField]
     private int _counter = 0;
+    [SerializeField]
+    private int _criticalClicks = 10;
+    [SerializeField]
+    private float _probabilityFactor = 0.01f;
 
     // Start is called before the first frame update
     private void Start()
     {
         Debug.Log("Hello World!");
+        UpdateTotalMuffins();
     }
 
     // Update is called once per frame
@@ -26,8 +31,32 @@ public class MyFirstScript : MonoBehaviour
 
     public void OnMuffinClicked()
     {
-        
-        _counter = _counter + 1;
-        Debug.Log(_counter);
+        if (Random.value < _probabilityFactor)
+        {
+            _counter = _counter + _criticalClicks;
+            Debug.Log("Critical Hit!");
+        }
+        else
+        {
+            _counter = _counter + 1;
+            //Debug.Log("Plus 1");
+        }
+
+        UpdateTotalMuffins();
+        //Debug.Log(_counter);
+    }
+
+    private void UpdateTotalMuffins()
+    {
+        if(_counter == 1)
+        {
+            _totalMuffinsText.text = _counter.ToString() + " muffin";
+        }
+        else
+        {
+            _totalMuffinsText.text = _counter.ToString() + " muffins";
+        }
+
+
     }
 }
--- Assets/MuffinClicker/Scripts/VolumeController.cs
+++ Assets/MuffinClicker/Scripts/VolumeController.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
+using UnityEngine.UI;
 
 public class VolumeController : MonoBehaviour
 {
+   //[SerializeField]
+   public Slider _volumeSlider;
+
    public void OnVolumeChanged()
     {
         Debug.Log("Volume Slider has been changed");
+        AudioListener.volume = _volumeSlider.value;
     }
 
 }
